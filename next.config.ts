b0f6cd0e589b9c5ce8f767b829bdb16c3de54eba
Segun Ojo <olusegunkd2017@gmail.com>
2026-02-22 04:48:49 +0100

    fix: directions

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "./frontend/node_modules/next";
-
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
-export default nextConfig;
